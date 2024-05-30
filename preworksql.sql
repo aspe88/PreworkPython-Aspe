1a994ef4b7a13af64aca21ac4eb573abdddb9a95
aspe88 <deaspecarlos@gmail.com>
2024-05-30 17:19:01 +0200

    reto otd

--- preworksql.sql
+++ preworksql.sql
@@ -1,3 +1,13 @@
-UPDATE public.students
-SET name = 'Jorge', age = 33
-WHERE id = 2;
+SELECT * FROM flights
+WHERE status = 'On Time'
+
+SELECT * FROM bookings
+WHERE total_amount > 1000000
+
+SELECT * FROM aircrafts_data
+
+SELECT flight_id FROM flights
+WHERE aircraft_code = '733'
+
+SELECT * FROM tickets
+Where passenger_name LIKE 'IRINA%'
